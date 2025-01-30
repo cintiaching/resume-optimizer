5b1facacb3618befe3ad74fc57a7bec318177ab1
cintiaching <swching.cintia@gmail.com>
2025-01-30 23:29:11 +0800

    add chat with local deepseek

---
+++ optimizer/llm.py
@@ -0,0 +1,35 @@
+import ollama
+from enum import Enum
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+class LLMs(Enum):
+    DEEPSEEK_R1 = "deepseek-r1:1.5b"
+
+
+def llm_chat(prompt: str, model_name: LLMs):
+    response = ollama.chat(model=model_name.value, messages=[
+        {
+            "role": "user",
+            "content": prompt,
+        },
+    ])
+    return response
+
+
+class TokenCounter:
+    def __init__(self):
+        self._count = 0
+
+    def __add__(self, other):
+        self._count += other
+        return self
+
+    def add(self, other):
+        self._count += other
+
+    def total_token_used(self):
+        return self._count
